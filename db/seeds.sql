d7d9f5027564388aaac8a7993d8c40c3fe7a41f1
Samantha <lovelychick1111@yahoo.com>
2018-03-11 22:45:36 -0400

    add file structure and some code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Susie Q', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Fred', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Sleep', false);
+
+
+
---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+
+var bodyparser = require('body-parser');
+
+var methodOverride = require('method-override')
